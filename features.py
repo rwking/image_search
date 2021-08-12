61f5dfa9f6903863629b56cbb88c4db84a783863
Rich King <rwking@gmail.com>
2021-08-11 21:48:03 -0400

    first

---
+++ features.py
@@ -0,0 +1,46 @@
+# Import the libraries
+import os
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.keras.models import Model
+from pathlib import Path
+from PIL import Image
+
+
+class FeatureExtractor:
+    def __init__(self):
+        # Use VGG-16 as the architecture and ImageNet for the weight
+        base_model = VGG16(weights='imagenet')
+        # Customize the model to return features from fully-connected layer
+        self.model = Model(inputs=base_model.input,
+                           outputs=base_model.get_layer('fc1').output)
+
+    def extract(self, img):
+        # Resize the image
+        img = img.resize((224, 224))
+        # Convert the image color space
+        img = img.convert('RGB')
+        # Reformat the image
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        # Extract Features
+        feature = self.model.predict(x)[0]
+        return feature / np.linalg.norm(feature)
+
+
+if __name__ == "__main__":
+    # Iterate through images (Change the path based on your image location)
+    for img in sorted(os.listdir("./images")):
+        # New feature extractor
+        fe = FeatureExtractor()
+        # Image path
+        img_path = "./images/{}".format(img) 
+        print(img_path)
+        # Extract Features
+        feature = fe.extract(img=Image.open(img_path))
+        # Save the Numpy array (.npy) on designated path
+        out = img.split('.')[0]
+        feature_path = "features/{}.npy".format(out)
+        np.save(feature_path, feature)
